0ff76b4c8eee0b3bedc91974b8563771659496b2
Gabriel Lamuño <gc2lgno@gmail.com>
2020-05-24 10:43:40 -0400

    Initial commit

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = new User();
+        $admin->name = "administrator";
+        $admin->email = "admin@admin.com";
+        $admin->password = Hash::make("admin");
+        $admin->save();
+    }
+}
